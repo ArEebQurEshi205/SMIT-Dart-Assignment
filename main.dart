e44aad4fdb7cf032f0171c3ca206e19e20d94145
ArEeb QurEshi <areebulameenq12345@gmail.com>
2023-12-31 01:56:38 +0500

    Introduction

--- main.dart
+++ main.dart
@@ -1,3 +1,10 @@
 void main(){
-  print("Areeb ul Amin Qureshi");
+  String name = "ArEeb Ul Amin QurEShi";
+  String course = "Flutter";
+  String rollno = "208604";
+  String batch = "2";
+  print("Name: $name");
+  print("Course: $course");
+  print("Roll No: $rollno");
+  print("Batch: $batch"); 
 }
