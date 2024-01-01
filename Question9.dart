0ee32f476b002355cbfa5f6e4127c63802b5a516
ArEeb QurEshi <areebulameenq12345@gmail.com>
2024-01-01 18:28:26 +0500

    question 9

---
+++ Question9.dart
@@ -0,0 +1,22 @@
+// Check if the number is even or odd, 
+// If number is even then check if this is divisible by 5 or not & 
+// if number is odd then check if this is divisible by 7 or not.
+
+void checkNumber(int number){
+  if(number % 2 == 0){
+    if(number % 5 == 0){
+      print('$number is even and divisible by 5.');
+    }
+    else{
+      print('$number is even but not divisible by 5.');
+    }
+  }
+  else{
+    if (number % 7 == 0){
+      print('$number is odd and divisible by 7.');
+    }
+    else{
+      print('$number is odd but not divisble by 7.');
+    }
+  }
+}
